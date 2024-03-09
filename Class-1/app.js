fd5aa46f8ede8916ae8e816b09f31791a4c47077
noman719 <mnomanmirz1996@gmail.com>
2024-03-09 23:56:52 +0500

    first commit

---
+++ Class-1/app.js
@@ -0,0 +1,18 @@
+// console.log("hey noman")
+// var aby = "noman"
+// alert(aby);
+
+// aby = "nomi"
+// alert(aby);
+// var name = 5
+// var newname = name + 5;
+// alert(newname);
+//  var originalNum = 23;
+//   var newNum = originalNum + 7;
+//   alert(newNum);
+
+var num = 5;
+var newNum = num--;
+alert(newNum);
+
+
---
+++ class-2/app.js
@@ -0,0 +1,147 @@
+// var userName = "noman"
+// console.log(userName)
+// var name1 = "noman"; 
+// var name2 = "khan";
+// console.log(name1 +" " + name2);
+
+// var num1 = 20
+// var num2 = 10
+// var num = num1 + num2;
+// console.log(num)
+// var num1 = 10
+// num1 = num1 + 3
+// console.log(num1)
+
+// num1 = num1 - "5"
+// console.log(num1)
+
+//  var num2 = 10
+//  console.log(num2++)
+//  console.log(num2)
+
+//  console.log(++num2)
+
+//  console.log(num1)
+
+//  num1 = 10
+//  console.log(num1)
+//  var result = num1 + ++num1 + num1++ + num1
+//  console.log(result)
+
+//  num1 = 10
+//  var result = ++num1 + ++num1 + num1 + num1++ + num1
+//  console.log(result)
+
+//  num1 = 10
+//  var result = ++num1 + --num1 + num1-- + num1 - ++num1
+//  console.log(result)
+
+
+//  var a = 15
+//  var result = a++ + ++a - --a + a-- + a -a -a++ + --a;
+//  console.log(result)
+
+
+
+//  var userName = prompt("enter your name", "noman")
+//  console.log(userName)
+
+//  var add = +prompt(num1 + num2)
+
+
+//   age = 20
+//   if(age == 18){
+//     console.log("Allow");
+//   }
+//   else {
+//     console.log("Not Allow")
+//   }
+
+//assigment -> Alerts
+// 1.
+// alert( "Hey!! Hope so you are doing Good. Nice to meet you" )
+
+// 2.
+// alert("Error! Please enter a valid password")
+
+// 3.
+// alert("Welcome to JS land add\n Happy coding")
+
+// 4.
+// alert("Welcome to JS land")
+// alert("Happy coding!! \n Prevent this page from creating additional dialogs.")
+
+// 5.
+// alert("Hello..I can run JS Through my web browser console.")
+
+// //variables and string
+
+// 1.
+// var username;
+
+// 2. 
+// var myName = "Noman Khan"
+
+// 3. 
+// var message = "Hello World";
+// alert(message);
+
+// 4.
+// var studentName = "Jhone doe"
+// alert(studentName);
+// var studentAge = "15 years old"
+// alert(studentAge);
+// var studentSkills = "Ceretified Mobile Application Developer"
+// alert(studentSkills)
+
+// 5.
+// var favfood = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(favfood);
+
+// 6.
+// var email = "My email is mnomanmirza1996@gmail.com"
+// alert(email);
+
+// 7.
+// var book = "I am trying to learn from the book A smarter \nway to learn JavaScript"
+// alert(book);
+
+// 8.
+// //on index.html
+
+// 9.
+// var art = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(art);
+
+// //variables for numbers
+
+// var age = "I am 27 years old"
+// alert(age);
+
+// 2.
+// var myVisit = "You visited this site 15 times"
+// alert(myVisit);
+
+// 3.
+// var birthYear = 1996;
+
+// 4. 
+// //on html page
+
+// //chapter#4
+// 1.
+// var myName, userName, myVisit;
+
+// 2.
+// //legal names
+// var username, myClass, $currency, myName, fullName;
+
+// //illegal names
+// var 2myName, 56, -username, if, class;
+
+3.
+//on html page
+
+//maths expressions
+
+1.
